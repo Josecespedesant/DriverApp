48563204b370a1ca7deb7aebf3bfc93840e4fcf5
dnsing <33098069+dnsing@users.noreply.github.com>
2018-11-15 00:26:59 -0600

    Se implementa el scanner de carne

---
+++ app/src/main/java/com/tec/driverapp/BarcodeScanner.java
@@ -0,0 +1,134 @@
+package com.tec.driverapp;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+import static android.Manifest.permission.CAMERA;
+
+public class BarcodeScanner extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+
+    private static final int RESQUEST_CAMERA = 1;
+    private ZXingScannerView scannerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkPermission()){
+                Toast.makeText(BarcodeScanner.this, "Permiso AK7", Toast.LENGTH_LONG).show();
+            }else{
+                requestPermission();
+            }
+        }
+    }
+
+    private boolean checkPermission(){
+        return
+                (ContextCompat.checkSelfPermission(BarcodeScanner.this, CAMERA) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA}, RESQUEST_CAMERA);
+    }
+    public void onRequestPermissionsResult(int requestCode, String permission[], int grantResults[]){
+        switch(requestCode){
+            case RESQUEST_CAMERA:
+                if(grantResults.length > 0){
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if(cameraAccepted){
+                        Toast.makeText(BarcodeScanner.this, "Permiso ak7",Toast.LENGTH_LONG).show();
+                        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                            if(shouldShowRequestPermissionRationale(CAMERA)){
+                                displayAlertMessage("Allow access",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                    requestPermissions(new String[]{CAMERA}, RESQUEST_CAMERA);
+                                                }
+                                            }
+                                        });
+                                return;
+                            }
+                        }
+                    }
+                }
+                break;
+        }
+    }
+    @Override
+    public  void onResume(){
+        super.onResume();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkPermission()){
+                if(scannerView == null){
+                    scannerView = new ZXingScannerView(this);
+                    setContentView(scannerView);
+                }
+                scannerView.setResultHandler(this);
+                scannerView.startCamera();
+            }else{
+                requestPermission();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+
+    }
+
+    public void displayAlertMessage(String message, DialogInterface.OnClickListener listener){
+        new AlertDialog.Builder(BarcodeScanner.this)
+                .setMessage(message)
+                .setPositiveButton("ok",listener)
+                .setNegativeButton("Cancel",null)
+                .create()
+                .show();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        final String myResult = result.getText();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan result");
+        builder.setPositiveButton("Listo", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent resultado = new Intent();
+                resultado.putExtra("resultado", myResult);
+                setResult(RESULT_OK, resultado);
+                finish();
+              //  scannerView.resumeCameraPreview(BarcodeScanner.this);
+            }
+        });
+        builder.setNeutralButton("Visit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(myResult));
+                startActivity(intent);
+            }
+        });
+        builder.setMessage(myResult);
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+}
--- app/src/main/java/com/tec/driverapp/RegistrationActivity.java
+++ app/src/main/java/com/tec/driverapp/RegistrationActivity.java
@@ -1,14 +1,17 @@
 package com.tec.driverapp;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class RegistrationActivity extends AppCompatActivity {
+    TextView carnet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +19,18 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
 
         final TextView volverainiciar = (TextView) findViewById(R.id.volverainiciarsesion);
+        final Button scanner = (Button) findViewById(R.id.scanner);
+        carnet = (TextView) findViewById(R.id.carnet);
 
+        carnet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(RegistrationActivity.this, BarcodeScanner.class);
+                startActivityForResult(registerIntent, 1);
+
+                //RegistrationActivity.this.startActivity(registerIntent);
+            }
+        });
         volverainiciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,5 +38,16 @@ public class RegistrationActivity extends AppCompatActivity {
                 RegistrationActivity.this.startActivity(registerIntent);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            if(resultCode == RESULT_OK){
+                String resultado = data.getStringExtra("resultado");
+                carnet.setText(resultado);
+            }
+        }
     }
 }
