032f4297c124ba5204c9a7eb15fafe1731fc6466
José Antonio Céspedes Downing <tonio782009@hotmail.com>
2018-11-14 20:37:19 -0600

    Se actualizan las interfaces login y registro

--- app/src/main/java/com/tec/driverapp/MainActivity.java
+++ app/src/main/java/com/tec/driverapp/MainActivity.java
@@ -1,21 +1,47 @@
 package com.tec.driverapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+
+            @SuppressLint("PackageManagerGetSignatures") PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.tec.driverapp",//give your package name here
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+
+                Log.d(TAG, "Hash ASLKJDNKJHNLASDOHASHIPDASDPOHIJASDPHO : " + Base64.encodeToString(md.digest(), Base64.NO_WRAP));//Key hash is printing in Log
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.d(TAG, e.getMessage(), e);
+        } catch (NoSuchAlgorithmException e) {
+            Log.d(TAG, e.getMessage(), e);
+        }
 
         final EditText nombreusuariologin = (EditText) findViewById(R.id.nombreusuariologin);
         final EditText contraseñalogin = (EditText) findViewById(R.id.passwordlogin);
@@ -29,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
                 Intent registerIntent = new Intent(MainActivity.this, RegistrationActivity.class);
                 MainActivity.this.startActivity(registerIntent);
             }
+
+
         });
     }
+
 }
