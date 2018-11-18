b98d16973b307b99b00d2f8bc2dc5bbaab8829f2
dnsing <33098069+dnsing@users.noreply.github.com>
2018-11-17 21:09:39 -0600

    Log in linkedIn v.1

--- app/src/main/java/com/tec/driverapp/MainActivity.java
+++ app/src/main/java/com/tec/driverapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -14,6 +15,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.linkedin.platform.LISessionManager;
+import com.linkedin.platform.errors.LIAuthError;
+import com.linkedin.platform.listeners.AuthListener;
+import com.linkedin.platform.utils.Scope;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -43,11 +49,20 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, e.getMessage(), e);
         }
 
-        final EditText nombreusuariologin = (EditText) findViewById(R.id.nombreusuariologin);
+        final TextView carnet = (TextView) findViewById(R.id.carnet);
         final EditText contraseñalogin = (EditText) findViewById(R.id.passwordlogin);
         final TextView registrate = (TextView) findViewById(R.id.registratetext);
         final ImageView linkedin = (ImageView) findViewById(R.id.linkedimg);
         final RelativeLayout iniciarsesion = (RelativeLayout) findViewById(R.id.iniciarsesion);
+
+        linkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(v);
+            }
+
+
+        });
 
         registrate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,6 +73,42 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
+
+        carnet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent barcode = new Intent(MainActivity.this, BarcodeScanner.class);
+                MainActivity.this.startActivity(barcode);
+            }
+
+
+        });
+    }
+
+    public void login(View view){
+        LISessionManager.getInstance(getApplicationContext()).init(this, buildScope(), new AuthListener() {
+            @Override
+            public void onAuthSuccess() {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                intent.putExtra("valor", LISessionManager.getInstance(getApplicationContext()).getSession().getAccessToken().toString());
+
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAuthError(LIAuthError error) {
+
+            }
+        },true);
+    }
+
+    private static Scope buildScope(){
+        return Scope.build(Scope.R_BASICPROFILE, Scope.R_EMAILADDRESS);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        LISessionManager.getInstance(getApplicationContext()).onActivityResult(this, requestCode, resultCode, data);
     }
 
 }
