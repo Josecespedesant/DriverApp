7a44c6626fd9db51d408c26061aef80ae0390c44
David <david.azoherra@gmail.com>
2018-11-22 01:56:39 -0600

    mejora comm

--- app/src/main/java/com/tec/comm/RegistrarConductor.java
+++ app/src/main/java/com/tec/comm/RegistrarConductor.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import com.google.gson.Gson;
 import com.tec.entities.Conductor;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -13,18 +14,30 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class RegistrarConductor {
+public class RegistrarConductor extends AsyncTask<Conductor, Void, Boolean> {
 
-    private final String registrarConductorURL = "http://192.168.100.7:8080/registro-conductor";
+    private final String registrarConductorURL = "http://localhost:8080/registro-conductor";
 
-    public boolean sendRegistro(Conductor conductor) {
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Boolean doInBackground(Conductor... conductors) {
+        boolean registrado = false;
+
+        Conductor conductor = conductors[0];
         Gson gson = new Gson();
         String json = gson.toJson(conductor);
+
         OutputStream out = null;
 
         try {
+            System.out.println("oh hi");
             URL url = new URL(this.registrarConductorURL);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            out = new BufferedOutputStream(urlConnection.getOutputStream());
 
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
             writer.write(json);
@@ -33,11 +46,13 @@ public class RegistrarConductor {
             out.close();
 
             urlConnection.connect();
+
+            registrado = true;
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return true;
+        return registrado;
     }
 }
